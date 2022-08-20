25b654a72ad7576a9dfa31d2aa79706956946e1b
valquero779 <47458199+valquero779@users.noreply.github.com>
2022-08-20 16:45:38 -0500

    Primer commit
    Creacion de los algoritmos de generacion de numeros pseudoaleatorio

---
+++ Algoritmo de cuadrados medios.py
@@ -0,0 +1,42 @@
+def numeroPseudo(result):
+
+    result = result ** 2
+
+
+    result=str(result)
+    while (len(result) >5):
+        result=result[1::]
+
+        result=result[:-1:]
+
+
+    return int(result)
+
+seed=int(input("Ingrese una semilla: "))
+ri=int(input("Cuantos numero quiere que se generen con esta semilla?: "))
+
+
+semillas=[]
+resultados=[]
+if seed.__sizeof__()<4:
+    print("No se pueden generar numeros por que la longitud de la semilla es menor de 4")
+else:
+    x = numeroPseudo(seed)
+    y = 10 ** len(str(x))
+    semillas.append(x)
+    resultados.append( "{:.4f}".format((x/y)))
+    for i in range(1,ri):
+
+        x=numeroPseudo(semillas[i - 1])
+        print(x)
+        y=10** len(str(x))
+        print(y)
+        resultados.append(x/y)
+        semillas.append(x)
+
+print(semillas)
+print(resultados)
+
+print("los resultados son: ")
+for x in resultados:
+    print(x)
---
+++ Algoritmo de productos medios.py
@@ -0,0 +1,49 @@
+def numeroPseudo(result,result2):
+
+    result=result*result2
+
+
+    result=str(result)
+    while (len(result) >5):
+        result=result[1::]
+
+        result=result[:-1:]
+
+
+    return int(result)
+
+seed=int(input("Ingrese una semilla: "))
+seed2= int(input("Ingrese la segunda semilla:"))
+ri=int(input("Cuantos numero quiere que se generen con esta semilla?: "))
+
+
+semillas=[]
+resultados=[]
+if seed.__sizeof__()<4:
+    print("No se pueden generar numeros por que la longitud de la semilla es menor de 4")
+else:
+    x = numeroPseudo(seed,seed2)
+    y = 10 ** len(str(x))
+    semillas.append(x)
+    resultados.append( "{:.4f}".format((x/y)))
+
+
+    antes=seed2
+    ahora=semillas[0]
+    for i in range(1,ri):
+
+        x=numeroPseudo(antes,ahora)
+        print(x)
+        y=10** len(str(x))
+        print(y)
+        resultados.append(x/y)
+        semillas.append(x)
+        antes=ahora
+        ahora=semillas[i]
+
+print(semillas)
+print(resultados)
+
+print("los resultados son: ")
+for x in resultados:
+    print(x)
